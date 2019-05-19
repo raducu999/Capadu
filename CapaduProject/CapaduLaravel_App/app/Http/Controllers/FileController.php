d7d83d45ccc7cdbdd0ba71ef31450a340e367029
raducu999 <radustefan1302@gmail.com>
2019-05-20 00:17:22 +0300

    upload

--- CapaduProject/CapaduLaravel_App/app/Http/Controllers/FileController.php
+++ CapaduProject/CapaduLaravel_App/app/Http/Controllers/FileController.php
@@ -6,25 +6,30 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 use App\File;
+use App\User;
 
 class FileController extends Controller
 {
     public function upload (Request $request) {
 
+        $user = User::find(Auth::user()->id);
+        $user->upload_activity = "nu s-a putut incarca materialul selectat, asigurativa ca ati selectat un material valid";
+        $user->save();
+
         $request->validate([
-
             'fileToUpload' => 'required',
-
-		]);
-
+        ]);
+        
         $fileName = time().'.'.request()->fileToUpload->getClientOriginalExtension();
 
         request()->fileToUpload->move(public_path('User_Files'), $fileName);
 
-        return response()->json(['success'=>'You have successfully upload file.']);
+        $user->upload_activity = "materialul a fost incarcat cu succes";
+        $user->save();
+
+        return;
         
     }
-
     public function delete ($id) {
         
     }
